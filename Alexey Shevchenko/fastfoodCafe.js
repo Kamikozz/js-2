bdf6b3a3789acab5de52a0efd9b7645b305b11b9
Alexey Shevchenko <kamikozz@mail.ru>
2019-01-09 01:26:38 +0300

    added deepFreeze instead of Obj.freeze + getters + restructured 'Order'

--- Alexey Shevchenko/fastfoodCafe.js
+++ Alexey Shevchenko/fastfoodCafe.js
@@ -15,6 +15,29 @@ var fastfoodcafe = (function () {
 var z = fastfoodcafe.c;
 
 /**
+ * Deep freeze instead of just Object.freeze.
+ * @param obj
+ * @returns {ReadonlyArray<any>}
+ */
+function deepFreeze(obj) {
+	// Get property names from obj
+	var propNames = Object.getOwnPropertyNames(obj);
+
+	// Freeze properties to freeze the object
+	propNames.forEach(function (name) {
+		var prop = obj[name];
+
+		// Freeze 'prop' if it is an object
+		if (typeof prop === 'object' && prop !== null) {
+			deepFreeze(prop);
+		}
+	});
+
+	// Freeze the object 'obj' (nothing will be wrong if it is already freezed)
+	return Object.freeze(obj);
+}
+
+/**
  * Fast-food cafe module.
  * @module fastfoodcafe
  */
@@ -22,11 +45,11 @@ var z = fastfoodcafe.c;
 /**
  * Class MenuItem
  *
- * @constructor
  * @param type Type can be 'Drink', 'Hamburger' or 'Salad'
  * @param name
  * @param price
  * @param calories
+ * @constructor
  */
 function MenuItem(type, name, price, calories) {
 	this.type = type;
@@ -35,11 +58,19 @@ function MenuItem(type, name, price, calories) {
 	this.calories = calories;
 }
 
-MenuItem.prototype.calculatePrice = function() {
+MenuItem.prototype.getType = function () {
+	return this.type;
+};
+
+MenuItem.prototype.getName = function () {
+	return this.name;
+};
+
+MenuItem.prototype.calculatePrice = function () {
 	return this.price;
 };
 
-MenuItem.prototype.calculateCalories = function() {
+MenuItem.prototype.calculateCalories = function () {
 	return this.calories;
 };
 
@@ -58,10 +89,15 @@ function Order() {
  * Shows items from an order.
  */
 Order.prototype.showOrder = function () {
-	console.log('Your order:\n +');
-	this.items.map(function (item) {
-		console.log('* ' + item.name);
-	});
+	if (this.items.length) {
+		console.log('Order:');
+		this.items.map(function (item) {
+			console.log('* ' + item.name);
+		});
+	}
+	else {
+		console.log('Order is empty!');
+	}
 };
 
 /**
@@ -70,6 +106,7 @@ Order.prototype.showOrder = function () {
  */
 Order.prototype.addItem = function (item) {
 	this.items.push(item);
+	return this;
 };
 
 /**
@@ -92,7 +129,7 @@ Order.prototype.deleteItem = function (item) {
  */
 Order.prototype.payOrder = function () {
 	this.isPaid = true;
-	Object.freeze(this);
+	deepFreeze(this);
 	console.log('Order was successfully payed and closed!');
 };
 
@@ -121,100 +158,136 @@ Order.prototype.calculateOrderCalories = function () {
 };
 
 Order.prototype.getBill = function () {
-	this.showOrder();
-	console.log('Total price: ' + this.calculateOrderPrice() + ' tugriks');
-	console.log('The energy value: ' + this.calculateOrderCalories() + ' calories');
-	console.log('-\nWaiting for the payment...');
+	if (this.isPaid) {
+		console.log('This order was payed earlier!');
+	}
+	else {
+		this.showOrder();
+		if (this.items.length) {
+			console.log('Total price: ' + this.calculateOrderPrice() + ' tugriks\n' +
+				'The energy value: ' + this.calculateOrderCalories() + ' calories\n' +
+				'-\nWaiting for the payment...');
+		}
+	}
+};
+
+Order.prototype.showOrderInfo = function () {
+	console.log('\n*** Order information ***');
+	if (this.isPaid) {
+		console.log('Status: \'PAYED\'');
+		this.showOrder();
+		console.log('Total price: ' + this.totalPrice + ' tugriks\n' +
+			'The energy value: ' + this.totalCalories + ' calories\n');
+	}
+	else {
+		console.log('Status: \'NOT PAYED\'');
+		this.showOrder();
+	}
 };
 
 
+// /**
+//  * Class, which objects describe hamburger's params.
+//  *
+//  * @constructor
+//  * @param size        Size
+//  * @param stuffing    Stuffing
+//  */
+// function Hamburger(size, stuffing) {
+//
+// 	this.size = size;
+//
+// }
+//
+// Hamburger.prototype = Object.create(MenuItem.prototype);
+// Hamburger.prototype.constructor = Hamburger;
+//
+// /* sizes, types of stuffings */
+// Hamburger.SIZE_SMALL = ...
+// Hamburger.SIZE_LARGE = ...
+// Hamburger.STUFFING_CHEESE = ...
+// Hamburger.STUFFING_SALAD = ...
+// Hamburger.STUFFING_POTATO = ...
+//
+// /**
+//  * Get hamburger's size
+//  */
+// Hamburger.prototype.getSize = function () {
+// 	return;
+// };
+//
+// /**
+//  * Get hamburger's stuffing
+//  */
+// Hamburger.prototype.getStuffing = function () {
+//
+// };
+//
+// /**
+//  * Get hamburger's price
+//  * @return {Number} Prince in tugriks
+//  */
+// Hamburger.prototype.calculatePrice = function () {
+//
+// };
+//
+// /**
+//  * Get hamburger's calorie
+//  * @return {Number} Calorie in calories
+//  */
+// Hamburger.prototype.calculateCalories = function () {
+//
+// };
 
-/**
- * Class, which objects describe hamburger's params.
- *
- * @constructor
- * @param size        Size
- * @param stuffing    Stuffing
- */
-function Hamburger(size, stuffing) {
-
-	this.size = size;
-
-}
-
-Hamburger.prototype = Object.create(MenuItem.prototype);
-Hamburger.prototype.constructor = Hamburger;
-
-/* sizes, types of stuffings */
-Hamburger.SIZE_SMALL = ...
-Hamburger.SIZE_LARGE = ...
-Hamburger.STUFFING_CHEESE = ...
-Hamburger.STUFFING_SALAD = ...
-Hamburger.STUFFING_POTATO = ...
-
-/**
- * Get hamburger's size
- */
-Hamburger.prototype.getSize = function () {
-	return;
-};
-
-/**
- * Get hamburger's stuffing
- */
-Hamburger.prototype.getStuffing = function () {
-
-};
-
-/**
- * Get hamburger's price
- * @return {Number} Prince in tugriks
- */
-Hamburger.prototype.calculatePrice = function () {
-
-};
-
-/**
- * Get hamburger's calorie
- * @return {Number} Calorie in calories
- */
-Hamburger.prototype.calculateCalories = function () {
-
-};
-
-
-/* CONST */
+/* constants of Cola, Coffee for class Drink */
 Drink.COLA = {
-	type: 'Drink',
 	name: 'Cola',
 	price: 50,
 	calories: 40
 };
 Drink.COFFEE = {
-	type: 'Drink',
 	name: 'Coffee',
 	price: 80,
 	calories: 20
 };
 
-
 /**
+ *
  * Class, which objects describe drink's params.
  *
+ * @param name Name of drink - 'Cola', 'Coffee'
  * @constructor
  */
 function Drink(name) {
-	switch(name) {
-		case 'Cola':
+	switch (name) {
+		case Drink.COLA.name:
+			MenuItem.call(this, 'Drink', name, Drink.COLA.price, Drink.COLA.calories);
 			break;
-		case '':
+		case Drink.COFFEE.name:
+			MenuItem.call(this, 'Drink', name, Drink.COFFEE.price, Drink.COFFEE.calories);
 			break;
 	}
-	MenuItem.call(this, name, name);
-	this.type = 'Drink';
 }
-
 
 Drink.prototype = Object.create(MenuItem.prototype);
 Drink.prototype.constructor = Drink;
 
+
+/* --- main entry point --- */
+var order1 = new Order();
+order1.getBill();
+order1.deleteItem(1);
+order1.addItem('cash').addItem('rules');
+order1.getBill();
+order1.payOrder();
+//order1.addItem('q');
+order1.showOrder();
+order1.getBill();
+order1.showOrderInfo();
+// order1.addItem('q');
+// order1.addItem('q');
+// order1.showOrder();
+// order1.getBill();
+// console.log(Object.isFrozen(order1));
+// order1.isPaid = false;
+// console.log(order1.isPaid);
